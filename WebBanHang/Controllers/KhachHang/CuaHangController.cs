540a5b7553d2320aca4d55ecb492d83c16f2a5f7
dangminhbk <dangminhbk@gmail.com>
2019-05-14 13:49:35 +0700

    small

--- WebBanHang/Controllers/KhachHang/CuaHangController.cs
+++ WebBanHang/Controllers/KhachHang/CuaHangController.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBanHang.Models;
 
 namespace WebBanHang.Controllers.KhachHang
 {
     public class CuaHangController : Controller
     {
         // GET: CuaHang
-        public ActionResult Index()
+        private WebHoa db = new WebHoa();
+        public ActionResult Index(int? page, int? pageSize)
         {
-            return View();
+            int skip = 0;
+            int take = 0;
+            int pageCurrent = 1;
+            int pageTotal = 1;
+            if (page != null && pageSize != null)
+            {
+                pageCurrent = (int)page;
+                skip = ((int)page - 1) * (int)pageSize;
+                take = (int)pageSize;
+            }
+            else
+            {
+                skip = 0;
+                take = 9;
+            }
+            var sanPhams = db.SanPhams.ToList();
+            pageTotal = sanPhams.Count() / take + 1;
+            var sanPhamsTrang = sanPhams.Skip(skip).Take(take).ToList();
+            ViewBag.pageCurrent = pageCurrent;
+            ViewBag.pageTotal = pageTotal;
+            return View(sanPhamsTrang);
         }
     }
 }
