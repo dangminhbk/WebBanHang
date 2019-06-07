ac3df51a449fbdf85cea6be412f9f06a05fd9396
dangminhbk <dangminhbk@gmail.com>
2019-06-07 21:56:22 +0700

    Lay ds SP dang Json

---
+++ WebBanHang/Controllers/KhachHang_React/API/SanPhamAPIController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebBanHang.Models;
+
+namespace WebBanHang.Controllers.KhachHang_React.Api
+{
+    public class SanPhamAPIController : ApiController
+    {
+        private WebHoa db = new WebHoa();
+
+        // GET: api/SanPhamAPI
+        public List<SanPham_simple> GetSanPhams()
+        {
+            //IQueryable<SanPham_simple> DanhSachSanPham = from item in db.SanPhams select new SanPham_simple(item);
+            var ds = new List<SanPham_simple>();
+            foreach (var item in db.SanPhams.ToList())
+            {
+                ds.Add(new SanPham_simple(item));
+            }
+            return ds;
+        }
+
+        // GET: api/SanPhamAPI/5
+        [ResponseType(typeof(SanPham))]
+        public IHttpActionResult GetSanPham(int id)
+        {
+            SanPham sanPham = db.SanPhams.Find(id);
+            if (sanPham == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(sanPham);
+        }        
+    }
+}
