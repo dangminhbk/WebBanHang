fac67df0ec7f72dc420de5ff9a86ee495d014577
dangminhbk <dangminhbk@gmail.com>
2019-06-15 00:37:14 +0700

    Them slider

--- WebBanHang/Controllers/Admin/DanhMucSanPhamsController.cs
+++ WebBanHang/Controllers/Admin/DanhMucSanPhamsController.cs
@@ -10,6 +10,7 @@ using WebBanHang.Models;
 
 namespace WebBanHang.Controllers.Admin
 {
+    [Authorize]
     public class DanhMucSanPhamsController : Controller
     {
         private WebHoa db = new WebHoa();
